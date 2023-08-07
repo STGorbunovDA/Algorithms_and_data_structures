9493831390068b1e3e6d7def7df187fe83732da4
STGorbunovDA <bous07@mail.ru>
2023-08-07 11:17:11 +0300

    BinaryTree

---
+++ HomeWork_4/BinaryTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using BinaryTree.Base;
+
+namespace BinaryTree
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+            Console.WriteLine("Префиксный обход дерева: ");
+            foreach (var item in tree.Preorder()) 
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Постфиксный обход дерева: ");
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Инфиксный обход дерева: ");
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
